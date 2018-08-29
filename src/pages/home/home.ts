bdabc6041e57c9789e52aca4020f40bd4716c2f8
Mahlangu1993 <212281972@tut4life.ac.za>
2018-08-29 15:46:13 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureAudioOptions,CaptureVideoOptions } from '@ionic-native/media-capture'; 
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private mediaCapture:MediaCapture) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  image(){
+   let options: CaptureImageOptions = { limit: 3 };
+    this.mediaCapture.captureImage(options)
+   .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+  }
+  video(){
+    let options: CaptureVideoOptions = { limit: 3 };
+    this.mediaCapture.captureVideo(options)
+   .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+  }
+  audio(){
+    let options: CaptureAudioOptions = { limit: 3 };
+    this.mediaCapture.captureAudio(options)
+   .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
   }
 
 }
